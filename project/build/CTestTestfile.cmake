603282fe3f4ca99198361f1363971a5278624f94
NicCaterino <caterino.2054849@studenti.uniroma1.it>
2024-08-29 17:13:52 +0200

    cmake debugging + project refactoring

---
+++ project/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/lattinone/Desktop/00_multi_robot_simulator/exercise
+# Build directory: /home/lattinone/Desktop/00_multi_robot_simulator/exercise/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ project/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/lattinone/Desktop/00_multi_robot_simulator/exercise/include/".split(';') if "/home/lattinone/Desktop/00_multi_robot_simulator/exercise/include/" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "mrsim"
+PROJECT_SPACE_DIR = "/home/lattinone/Desktop/00_multi_robot_simulator/exercise/build/devel"
+PROJECT_VERSION = "1.0.0"
